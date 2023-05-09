f61ce2ebe51a43c08f3423162d34b28ea0d614e8
user <user@DESKTOP-G8JIBFH>
2023-05-10 04:42:52 +0900

    초기 커밋.

---
+++ Assignment3-2/LOL.h
@@ -0,0 +1,21 @@
+#pragma once
+class LOL {
+public:
+	//constructor
+	LOL() {};
+
+	LOL(int N1, int N2, float a, float b, float x0, float dx, int m) {
+		fn1(N1, N2);
+		fn2(a, b, x0, dx, m);
+	}
+
+	//member data
+	int* nn;
+	float* mm;
+
+	//member function
+	void fn1(int, int);
+	void fn2(float, float, float, float, int);
+	void fileopen(int, int, float, float, float, float, int, string);
+};
+
